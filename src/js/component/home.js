713d167dfb22afa4d47937dec680cc6d8051c2c3
Hugo Rojas <hurojas.rios@gmail.com>
2019-08-20 06:43:01 +0000

    modal creado falta eliminar checkbox y validaciones de color y fecha

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InputTarea from "./inputTarea.js";
 import Lista from "./lista.js";
+import Modal from "./modal.js";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -19,7 +20,7 @@ export class Home extends React.Component {
 		this.obtenerListado = this.obtenerListado.bind(this);
 	}
 
-	/*cambiarvalor(valor) {
+	cambiarvalor(valor) {
 		let nuevo = this.state.datosLista;
 		fetch(
 			"https://3000-f3f09454-7985-4bfc-b8e6-01bceb2fc044.ws-us0.gitpod.io/api/todo/" +
@@ -52,8 +53,7 @@ export class Home extends React.Component {
 	}
 	componentDidMount() {
 		this.obtenerListado();
-	*/
-
+	}
 	borrar(valor) {
 		let eliminar = this.state.datosLista;
 		eliminar = eliminar.filter(item => {
@@ -67,6 +67,9 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="container ">
+				<h1>Tareas por hacer</h1>
+				<p className="float-right ">filter</p>
+				<i className="fas fa-filter float-right " />
 				<div className="input ">
 					<InputTarea valor={this.cambiarvalor} />
 				</div>
@@ -74,7 +77,24 @@ export class Home extends React.Component {
 					borrar={this.borrar}
 					datosLista={this.state.datosLista}
 				/>
-				<div className="center bg-dark " />
+				<div
+					className="card border-dark mb-3 text-center
+					">
+					<div className="card-header">Header</div>
+					<div className="card-body text-dark">
+						<h5 className="card-title">Agregar Tarea</h5>
+						<p className="card-text">
+							<button
+								type="button"
+								className="btn btn-dark"
+								data-toggle="modal"
+								data-target="#exampleModal">
+								<i className="fas fa-plus" />
+							</button>
+						</p>
+						<Modal />
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/js/component/lista.js
+++ src/js/component/lista.js
@@ -5,22 +5,26 @@ const Lista = props => {
 	const li = props.datosLista.map((item, i) => {
 		return (
 			<div key={i}>
-				<div
-					className="card text-white bg-info mb-3"
-					style="max-width: 18rem;">
-					<div className="card-header">Header</div>
-					<div className="card-body">
-						<h5 className="card-title">Info card title</h5>
-						<p className="card-text">{item.label}</p>
+				<a className="list-group-item list-group-item-action ">
+					<div className="card text-white bg-info mb-3">
+						<div className="card-header">Header</div>
+						<div className="card-body">
+							<h5 className="card-title">Info card title</h5>
+							<p className="card-text">{item.label}</p>
+							<input
+								type="checkbox"
+								aria-label="Checkbox for following text input"
+							/>
+							<a
+								className="float-right text-secondary"
+								onClick={() => {
+									props.borrar(item);
+								}}>
+								<i className="fas fa-times" />
+							</a>
+						</div>
 					</div>
-					<a
-						className="float-right text-secondary"
-						onClick={() => {
-							props.borrar(item);
-						}}>
-						<i className="fas fa-times" />
-					</a>
-				</div>
+				</a>
 			</div>
 		);
 	});
